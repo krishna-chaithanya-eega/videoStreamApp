a22941a40de330e211d8527aaf1c9a78c35955fc
Krishna Chaithanya Eega <48961874+krishna-chaithanya-eega@users.noreply.github.com>
2021-05-31 10:54:36 +0530

    API added

--- src/apis/streams.js
+++ src/apis/streams.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
 export default axios.create({
-    baseURL:"http://localhost:3001"
+    baseURL:"https://my-json-server.typicode.com/krishna-chaithanya-eega/videoStreamAPI"
 
 })
